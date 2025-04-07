c5abbd3d3514967d6fde536c8a669e6a3dc8cc27
bakpaul <paul.baksic@outlook.fr>
2025-04-07 14:27:18 +0200

    Specialized desc working, need to fix preset

--- scripts/subProjectLister/exportPlugins.py
+++ scripts/subProjectLister/exportPlugins.py
@@ -5,12 +5,15 @@ SOFA_SRC=os.environ['SOFA_SRC']
 filenames=[f"{SOFA_SRC}/applications/plugins/CMakeLists.txt",f"{SOFA_SRC}/applications/projects/CMakeLists.txt"]
 presetFilename=f"{SOFA_SRC}/CMakePresets.json"
 descriptionFolder=f"{SOFA_SRC}/scripts/subProjectLister/AdvancedDescriptions/"
+presetFilname=f"{SOFA_SRC}/CMakePresets.json"
 
 possibleTypes=["application","folder","plugin"]
 
 applications = []
 folders = []
 plugins = []
+
+presetLookup = PresetLookup(presetFilname)
 
 def saveProject(proj:Projects):
     global applications, folders, plugins
@@ -26,23 +29,19 @@ def saveProject(proj:Projects):
 ## Extract all subprojects
 lines = []
 for filename in filenames:
+    cmakeFolderRelativePath = filename.removeprefix(f"{SOFA_SRC}/").removesuffix("CMakeLists.txt")
     with open(filename) as file:
-        lines = lines + [line.rstrip() for line in file]
+        ## Transform into Projects type and sort them regarding type
+        for line in [line.rstrip() for line in file]:
+            if("sofa_add_subdirectory" in line):
+                arguments = line.split('(')[1].split(')')[0]
+                proj = InternalProjects.createFromArguments(arguments, cmakeFolderRelativePath, descriptionFolder)
+                saveProject(proj)
+            elif("sofa_add_external" in line):
+                arguments = line.split('(')[1].split(')')[0]
+                proj = ExternalProjects.createFromArguments(arguments,descriptionFolder)
+                saveProject(proj)
 
-## Transform into Projects type and sort them regarding type
-for line in lines:
-    if("sofa_add_subdirectory" in line):
-        arguments = line.split('(')[1].split(')')[0]
-        proj = InternalProjects.createFromArguments(arguments)
-        proj.extractPresets(presetFilename)
-        proj.extractAdvancedDescription(descriptionFolder)
-        saveProject(proj)
-    elif("sofa_add_external" in line):
-        arguments = line.split('(')[1].split(')')[0]
-        proj = ExternalProjects.createFromArguments(arguments)
-        proj.extractPresets(presetFilename)
-        proj.extractAdvancedDescription(descriptionFolder)
-        saveProject(proj)
 
 
 ## Sort list regarding the name of the project
--- scripts/subProjectLister/utils.py
+++ scripts/subProjectLister/utils.py
@@ -1,4 +1,35 @@
+import os.path
+
 import numpy as np
+import json
+import pathlib
+
+
+class PresetLookup():
+    presetsDict : dict
+    def __init__(self, filename):
+        with open(filename, 'r',encoding='utf-8-sig') as f:
+            self.presetsDict = json.load(f)
+
+    def getUsingPresetNames(self, subPresetName : str, presetList : list[str]):
+        # Find presets that include it
+        for preset in self.presetsDict["configurePresets"]:
+            if "inherits" in  preset and subPresetName in preset["inherits"]:
+                if "hidden" not in preset or not preset["hidden"]:
+                    print(preset["name"])
+                    presetList.append(preset["name"])
+                self.getUsingPresetNames(preset["name"],presetList)
+
+    def getListOfPResetsContainingProject(self, projectCorrectedName: str):
+        presetNames = []
+        for preset in self.presetsDict["configurePresets"]:
+            for varName in preset["cacheVariables"].keys():
+                if projectCorrectedName in varName:
+                    currPresetName = preset["name"]
+                    if "hidden" not in preset or not preset["hidden"]:
+                        print(currPresetName)
+                        presetNames.append(currPresetName)
+                    self.getUsingPresetNames(currPresetName,presetNames)
 
 class Projects():
 
@@ -7,22 +38,26 @@ class Projects():
     presetList = list[str]
     advancedDescription = str
 
-    def __init__(self, name, type):
+    def __init__(self, name, type, advancedDescriptionFolder=""):
         self.name = name
         self.type = type
         self.presetList = []
-        self.advancedDescription = ""
+        self.extractAdvancedDescription(advancedDescriptionFolder)
 
     def getCorrectedName(self):
         return self.name.upper().replace('.','_')
 
     def extractAdvancedDescription(self,folderPath):
-        #TODO
         self.advancedDescription = ""
+        potentialDescriptionFile = pathlib.Path(os.path.join(folderPath,self.name))
+        if potentialDescriptionFile.exists():
+            for line in  open(potentialDescriptionFile.absolute(), 'r').readlines():
+                self.advancedDescription += line + " "
 
-    def extractPresets(self,presetFilename):
-        #TODO
-        self.presetList = []
+
+    def extractPresets(self,presets : PresetLookup):
+        self.presetList = presets.getListOfPResetsContainingProject(self.getCorrectedName())
+
 
     def getUpperType(self):
         return self.type.upper()
@@ -46,18 +81,23 @@ class Projects():
 
 class InternalProjects(Projects):
 
-    def __init__(self, name, type):
-        Projects.__init__(self,name,type)
+    def __init__(self, name, type, cmakeFolderRelativePath,advancedDescriptionFolder=""):
+        Projects.__init__(self,name,type, advancedDescriptionFolder)
+        self.link = "https://github.com/sofa-framework/sofa/tree/master/" + cmakeFolderRelativePath + name
 
     @staticmethod
-    def createFromArguments(stringArg : str):
+    def createFromArguments(stringArg : str, cmakeFolderRelativePath, advancedDescriptionFolder=""):
         argList = stringArg.split()
-        return InternalProjects(argList[1], argList[0])
+        return InternalProjects(argList[1], argList[0], cmakeFolderRelativePath, advancedDescriptionFolder=advancedDescriptionFolder)
 
     def getDisplayName(self):
-        return f"{self.name}"
+        return f"[{self.name}]({self.link})"
+
 
     def getDescription(self):
+        if self.advancedDescription != "":
+            return self.advancedDescription
+
         return f"{self.type.capitalize()} named {self.name}"
 
     def getActvation(self):
@@ -69,27 +109,30 @@ class ExternalProjects(Projects):
     default_tag : str
     default_repo : str
 
-    def __init__(self, name, type, default_tag,default_repo):
-        Projects.__init__(self,name,type)
+    def __init__(self, name, type, default_tag,default_repo,advancedDescriptionFolder=""):
+        Projects.__init__(self,name,type,advancedDescriptionFolder)
         self.default_tag = default_tag
         self.default_repo = default_repo
 
     @staticmethod
-    def createFromArguments(stringArg : str):
+    def createFromArguments(stringArg : str, advancedDescriptionFolder=""):
         argList = stringArg.split()
         tagId = argList.index("GIT_REF") + 1
         repoId = argList.index("GIT_REPOSITORY") + 1
-        return ExternalProjects(argList[1], argList[0], argList[tagId], argList[repoId])
+        return ExternalProjects(argList[1], argList[0], argList[tagId], argList[repoId], advancedDescriptionFolder = advancedDescriptionFolder)
 
     def getDisplayName(self):
         return f"[{self.name}]({self.default_repo})"
 
     def getDescription(self):
-        return f"External {self.type.capitalize()} named {self.name} that needs ot be fetched"
+        if self.advancedDescription != "":
+            return self.advancedDescription
+        return f"External {self.type.capitalize()} named {self.name} that needs to be fetched"
 
     def getActvation(self):
         #TODO add presets
         return f"To fetch it set CMake flag 'SOFA_FETCH_{self.getCorrectedName()}' to ON, then activate it by setting CMake flag '{self.getUpperType()}_{self.getCorrectedName()}' to ON"
+
 
 
 def printTableFromProjectListToString(projectList:Projects):
@@ -98,8 +141,14 @@ def printTableFromProjectListToString(projectList:Projects):
         output += f"{project.displayDescription()}\n"
     return output
 
+
 def sortProjectByNames(projectList:Projects):
     names = [proj.name for proj in projectList]
     sortedIdx = np.argsort(names).tolist()
     return [projectList[i] for i in sortedIdx]
 
+if __name__ == "__main__":
+    lookup = PresetLookup("../../CMakePresets.json")
+    outList = []
+    lookup.getUsingPresetNames("RUNSOFA",outList)
+    print(outList)
