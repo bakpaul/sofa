06078a8bd250ea289329c92ffdd315bed8cf1144
Paul Baksic <paul.baksic@outlook.fr>
2025-04-09 14:48:46 +0200

    Fix names

--- scripts/subProjectLister/generateBothDocPages.sh
+++ scripts/subProjectLister/generateBothDocPages.sh
@@ -8,4 +8,4 @@ cp inTree-plugins.md 50_Activate_Plugins.md
 python3 exportInTreePlugins.py >> 50_Activate_Plugins.md
 
 cp supported-plugins.md 45_Suported_Plugins_List.md
-python3 exportSupportedPlugins.py >> 45_Suuported_Plugins_List.md
+python3 exportSupportedPlugins.py >> 45_Suported_Plugins_List.md
