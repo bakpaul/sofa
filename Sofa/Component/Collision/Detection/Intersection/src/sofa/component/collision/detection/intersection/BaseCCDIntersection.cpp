71ca02f5880e8650029850bb9250d11e5b6affe0
Paul Baksic <paul.baksic@outlook.fr>
2025-12-08 18:01:19 +0100

    Complete the line line collision detection, need to add unit tests now

--- Sofa/Component/Collision/Detection/Intersection/src/sofa/component/collision/detection/intersection/BaseCCDIntersection.cpp
+++ Sofa/Component/Collision/Detection/Intersection/src/sofa/component/collision/detection/intersection/BaseCCDIntersection.cpp
@@ -28,6 +28,14 @@ namespace sofa::component::collision::detection::intersection
 
 using namespace sofa::component::collision::geometry;
 
+double projectPointToEdge(const type::Vec3& point, const type::Vec3& edgeA, const type::Vec3& edgeB )
+{
+    const auto v = edgeB - edgeA;
+    const double vnorm = v.norm();
+    return type::dot(v,point - edgeA)/(vnorm * vnorm);
+}
+
+
 BaseCCDIntersection::BaseCCDIntersection()
     : d_contactDistance(initData(&d_contactDistance, 0.5_sreal, "contactDistance", "Distance below which a contact is created"))
 {
@@ -53,11 +61,6 @@ bool BaseCCDIntersection::testIntersection(Cube& cube1, Cube& cube2, const core:
     const SReal EdgeDist = Cube1MaxDist + Cube2MaxDist + alarmDist;
 
     type::Vec2 baryCoords(type::NOINIT);
-    sofa::geometry::Edge::closestPointWithEdge(Cube1MotionEdgeA,
-                                               Cube1MotionEdgeB,
-                                               Cube2MotionEdgeA,
-                                               Cube2MotionEdgeB, baryCoords);
-
 
     double v1v2 = dot(Cube2MotionEdge, Cube1MotionEdge);
     type::Vec3 A1A2 = Cube1MotionEdgeA - Cube2MotionEdgeA;
@@ -66,23 +69,62 @@ bool BaseCCDIntersection::testIntersection(Cube& cube1, Cube& cube2, const core:
     baryCoords[0] = (dot(Cube1MotionEdge,Cube1MotionEdge) * b[0] + v1v2 * b[1])/det;
     baryCoords[1] = (v1v2 * b[0]                                 + dot(Cube2MotionEdge,Cube2MotionEdge) * b[1])/det;
 
+    bool projectFirst = (baryCoords[0] < 0) || (baryCoords[0] > 1);
+    bool projectSecond = (baryCoords[1] < 0) || (baryCoords[1] > 1);
 
     if (baryCoords[0] < 0) baryCoords[0] = 0;
     if (baryCoords[0] > 1) baryCoords[0] = 1;
     if (baryCoords[1] < 0) baryCoords[1] = 0;
     if (baryCoords[1] > 1) baryCoords[1] = 1;
 
-    std::cout<<"BaryCoord : "<<baryCoords<<std::endl;
-    std::cout<<"Cube1 Edge length : "<<(Cube1MotionEdge).norm()<<std::endl;
-    std::cout<<"Cube1MotionEdgeA : "<<(Cube1MotionEdgeA)<<std::endl;
-    std::cout<<"Cube1MotionEdgeB : "<<(Cube1MotionEdgeB)<<std::endl;
-    std::cout<<"Cube2 Edge length : "<<(Cube2MotionEdge).norm()<<std::endl;
-    std::cout<<"Cube2MotionEdgeA : "<<(Cube2MotionEdgeA)<<std::endl;
-    std::cout<<"Cube2MotionEdgeB : "<<(Cube2MotionEdgeB)<<std::endl;
-    std::cout<<"EdgeDist : "<<EdgeDist<<std::endl;
-    std::cout<<"CurrentDist : "<<(   (Cube1MotionEdgeA + baryCoords[0] * Cube1MotionEdge )
-                                      - (Cube2MotionEdgeA + baryCoords[1] * Cube2MotionEdge )).norm()<<std::endl<<std::endl;
+    if (projectFirst || projectSecond)
+    {
 
+        const auto oldFirstPoint = Cube1MotionEdgeA + Cube1MotionEdge * baryCoords[0];
+        const auto oldSecondPoint = Cube2MotionEdgeA + Cube2MotionEdge * baryCoords[1];
+        if (projectFirst && projectSecond)
+        {
+            double newFirstBary = projectPointToEdge(oldSecondPoint, Cube1MotionEdgeA, Cube1MotionEdgeB);
+            if ( newFirstBary > 1 ) newFirstBary = 1;
+            if ( newFirstBary < 1 ) newFirstBary = 0;
+
+            double newSecondBary = projectPointToEdge(oldFirstPoint, Cube2MotionEdgeA, Cube2MotionEdgeB);
+            if ( newSecondBary > 1 ) newSecondBary = 1;
+            if ( newSecondBary < 1 ) newSecondBary = 0;
+
+            if (  ((1-newSecondBary) * Cube2MotionEdgeA + newSecondBary * Cube2MotionEdgeB - oldFirstPoint ).norm()
+                < ((1-newFirstBary ) * Cube1MotionEdgeA + newFirstBary  * Cube1MotionEdgeB - oldSecondPoint ).norm())
+            {
+                baryCoords[1] = newSecondBary;
+            }
+            else
+            {
+                baryCoords[0] = newFirstBary;
+            }
+        }
+        else if (projectFirst)
+        {
+            double newSecondBary = projectPointToEdge(oldFirstPoint, Cube2MotionEdgeA, Cube2MotionEdgeB);
+            if ( newSecondBary > 1 ) newSecondBary = 1;
+            if ( newSecondBary < 1 ) newSecondBary = 0;
+            if (  ((1-newSecondBary) * Cube2MotionEdgeA + newSecondBary * Cube2MotionEdgeB - oldFirstPoint ).norm()
+                < (oldFirstPoint - oldSecondPoint).norm())
+            {
+                baryCoords[1] = newSecondBary;
+            }
+        }
+        else if (projectSecond)
+        {
+            double newFirstBary = projectPointToEdge(oldSecondPoint, Cube1MotionEdgeA, Cube1MotionEdgeB);
+            if ( newFirstBary > 1 ) newFirstBary = 1;
+            if ( newFirstBary < 1 ) newFirstBary = 0;
+            if (  ((1-newFirstBary) * Cube1MotionEdgeA + newFirstBary * Cube1MotionEdgeB - oldSecondPoint ).norm()
+                < (oldFirstPoint - oldSecondPoint).norm())
+            {
+                baryCoords[0] = newFirstBary;
+            }
+        }
+    }
 
     return (   (Cube1MotionEdgeA + baryCoords[0] * Cube1MotionEdge )
              - (Cube2MotionEdgeA + baryCoords[1] * Cube2MotionEdge )).norm() < EdgeDist;
